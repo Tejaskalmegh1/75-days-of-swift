ec89b54c55b2634fe4c692dce06f7c33889a8756
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-01-11 22:33:56 +0530

    feat(day06): add examples using switch statement in swift

---
+++ day06/switch.swift
@@ -0,0 +1,32 @@
+// Day 6: switch statement in Swift
+
+let dayNumber = 3
+
+switch dayNumber {
+case 1:
+    print("Monday")
+case 2:
+    print("Tuesday")
+case 3:
+    print("Wednesday")
+case 4:
+    print("Thursday")
+case 5:
+    print("Friday")
+default:
+    print("Weekend")
+}
+
+// switch with String
+let grade = "A"
+
+switch grade {
+case "A":
+    print("Excellent")
+case "B":
+    print("Good")
+case "C":
+    print("Average")
+default:
+    print("Needs Improvement")
+}
