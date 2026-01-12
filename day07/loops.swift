b5aa91b2a6715ca364d2d82616cb0793f8263fc9
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-01-12 16:31:39 +0530

    feat(day07): add examples for for-in and while loops in swift

---
+++ day07/loops.swift
@@ -0,0 +1,21 @@
+// Day 7: Loops in Swift
+
+// for-in loop
+for number in 1...7 {
+    print("Number:", number)
+}
+
+// for-in with array
+let fruits = ["Apple", "Banana", "Mango"]
+
+for fruit in fruits {
+    print("Fruit:", fruit)
+}
+
+// while loop
+var counter = 1
+
+while counter <= 10 {
+    print("Counter:", counter)
+    counter += 1
+}
